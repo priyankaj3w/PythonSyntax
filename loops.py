62383fe27cb8c3c59e907f2a49e4bdb05f8bea5c
Priyanka Juyal <juyal.priyanka94@gmail.com>
2022-07-13 18:28:34 +0530

    Updated Code

---
+++ loops.py
@@ -0,0 +1,8 @@
+# This is a sample Python script.
+
+def for_loop(fruits):
+    i = 1
+    for fruit in fruits:
+        print(str(i)+' Fruit is :', fruit)
+        i = i+1
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
 import sys
+import loops
 
 
 def print_hi(name):
@@ -13,14 +14,31 @@ def working_with_list():
     print('Reverse:' + y)
 
 
+def working_with_list():
+    var_tuple = ('apple', 'mango', 'guava', 'orange', 'banana')
+    tiny_tuple = (123, 'john')
+    print('Tuple is:')
+    print(var_tuple+tiny_tuple)
+    loops.for_loop(var_tuple)
+
+
 def working_with_dict():
     tiny_dict = {'name': 'john', 'code': 6734, 'dept': 'sales'}
+    print('Dictionary is:')
     print(tiny_dict)
     print(tiny_dict.keys())
-    print(tiny_dict.values()) (union bnak of indai, bank of baroda, HDFC and Kotak)
+    print(tiny_dict.values())
+
+
+def decision_making(a, b):
+    if a > b:
+        print('a is greater')
+    else:
+        print('b is greater')
 
 
 if __name__ == '__main__':
     print_hi("Okay")
     working_with_list()
     working_with_dict()
+    decision_making(4, 10)
